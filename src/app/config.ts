f382cbfec6bbb0942ec9ddeb7eb653625b8919fe
Anousack789 <no69station@gmail.com>
2023-03-28 16:33:26 +0700

    change config

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,6 @@
 export class Config {
-  public static apiUrl = 'http://10.0.3.4:3000/';
-  public static socketUrl = 'http://10.0.3.4:3000/chatgpt';
+  //public static apiUrl = 'http://10.0.3.4:3000/';
+  //public static socketUrl = 'http://10.0.3.4:3000/chatgpt';
+  public static apiUrl = 'https://api-gpt.kokkoksole.com/';
+  public static socketUrl = 'https://api-gpt.kokkoksole.com/chatgpt';
 }
