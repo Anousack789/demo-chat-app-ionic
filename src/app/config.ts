4a93c30737ab41ce24c573dd148d8c8cf8081956
Anousack789 <no69station@gmail.com>
2023-03-30 09:06:55 +0700

    nono

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 export class Config {
   //public static apiUrl = 'http://192.168.42.20:3000/';
   //public static socketUrl = 'http://192.168.42.20:3000/chatgpt';
-  public static apiUrl = 'https://api-gpt.kokkoksole.com/';
-  public static socketUrl = 'https://api-gpt.kokkoksole.com/chatgpt';
+  public static apiUrl = 'https://api-chatgpt.kokkoksole.com/';
+  public static socketUrl = 'https://api-chatgpt.kokkoksole.com/chatgpt';
 }
